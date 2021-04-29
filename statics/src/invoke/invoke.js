29ba88cd0d085a9b2ee9ea8ffbc819775e5b056a
realityone <realityone@me.com>
2021-04-29 16:58:56 +0800

    set request body with input schema

--- statics/src/invoke/invoke.js
+++ statics/src/invoke/invoke.js
@@ -6,31 +6,30 @@ import 'codemirror/mode/javascript/javascript.js';
 import 'codemirror/mode/shell/shell.js';
 
 var setupCodeMirror = function () {
-    var requestBodyEditor = CodeMirror.fromTextArea(requestBody, {
+    window.requestBodyEditor = CodeMirror.fromTextArea(requestBody, {
         lineNumbers: true,
         mode: { name: "javascript", json: true },
     });
-    requestBodyEditor.setSize('100%', '300px');
-    var previewEditor = CodeMirror.fromTextArea(preview, {
+    window.requestBodyEditor.setSize('100%', '300px');
+    window.previewEditor = CodeMirror.fromTextArea(preview, {
         lineNumbers: true,
         mode: "shell",
     });
-    previewEditor.setSize('100%', '100%');
-    var responseBodyEditor = CodeMirror.fromTextArea(responseBody, {
+    window.previewEditor.setSize('100%', '100%');
+    window.responseBodyEditor = CodeMirror.fromTextArea(responseBody, {
         lineNumbers: true,
         mode: { name: "javascript", json: true },
     });
-    responseBodyEditor.setSize('100%', '100%');
+    window.responseBodyEditor.setSize('100%', '100%');
 };
 
 var fillMethod = function () {
-    console.log("AAAA");
     var methods = document.getElementsByClassName("service-method");
     for (const m of methods) {
         m.onclick = function () {
-            console.log("AAAA");
             var methodNameInput = document.getElementById("method-name");
             methodNameInput.value = m.dataset.methodName;
+            window.requestBodyEditor.setValue(m.dataset.inputSchema);
         }
     }
 };
